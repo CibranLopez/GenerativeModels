Resolve bug in loss calculation

libraries/model.py
@@ -412,9 +412,8 @@ def get_graph_losses(graph1, graph2, batch_size):
     Processing that in batches or subsets of nodes/edges can be more appropriate.
 
     Args:
-        graph1     (torch_geometric.data.Data): The first input graph.
-        graph2     (torch_geometric.data.Data): The second input graph.
-        batch_size (int):                       Size of the data batch, used to compute the MSE loss.
+        graph1 (torch_geometric.data.Data): The first input graph.
+        graph2 (torch_geometric.data.Data): The second input graph.
 
     Returns:
         node_losses (list with torch.Tensor inside): Loss value for node features between the two graphs.
@@ -426,19 +425,15 @@ def get_graph_losses(graph1, graph2, batch_size):
     edge_criterion = nn.MSELoss()
 
     # Calculate the loss for node features by comparing the node attribute tensors
-    node_losses_batch = []
+    node_losses = []
     for i in range(graph1.x.size(1)):
         node_loss = node_criterion(graph1.x[:, i],
                                    graph2.x[:, i])
-        node_losses_batch.append(node_loss)
+        node_losses.append(node_loss)
 
     # Calculate the loss for edge attributes by comparing the edge attribute tensors
     edge_loss = edge_criterion(graph1.edge_attr,
                                graph2.edge_attr)
-
-    # Divide by the number of data graphs in the batch
-    node_losses = [node_loss / batch_size for node_loss in node_losses_batch]
-    edge_loss  /= batch_size
 
     return node_losses, edge_loss
 