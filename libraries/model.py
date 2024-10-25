1d4c53736b36b9c3b20f3ecb29f5993d600552ce
cibran <cibranlpz@gmail.com>
2024-10-25 12:58:18 +0200

    Hard optimization on predict_noise.

--- libraries/model.py
+++ libraries/model.py
@@ -196,7 +196,7 @@ def denoise(batch_t, n_t_steps, node_model, edge_model, n_graph_embbedings, s=1e
     batch_size_0 = batch_0.num_graphs
 
     # Save graph-level embeddings
-    embedding_batch_0 = [g_batch_0[idx].y for idx in range(batch_size_0)] 
+    embedding_batch_0 = [batch_0[idx].y for idx in range(batch_size_0)]
 
     # Define t_steps as inverse of the diffuse process
     t_steps = torch.range(1,  n_t_steps+1, device=device)[::-1]
@@ -269,9 +269,7 @@ def predict_noise(g_batch_t, node_model, edge_model):
     """
 
     # Perform a single forward pass for predicting node features
-    out_x = node_model(g_batch_t.x,
-                       g_batch_t.edge_index,
-                       g_batch_t.edge_attr)
+    out_x = node_model(g_batch_t.x, g_batch_t.edge_index, g_batch_t.edge_attr)
 
     # Define x_i and x_j as features of every corresponding pair of nodes (same order than attributes)
     x_i = g_batch_t.x[g_batch_t.edge_index[0]]
@@ -279,37 +277,20 @@ def predict_noise(g_batch_t, node_model, edge_model):
     
     # Perform a single forward pass for predicting edge attributes
     # Introduce previous edge attributes as features as well
-    out_attr = edge_model(x_i, x_j, g_batch_t.edge_attr)
+    out_attr = edge_model(x_i, x_j, g_batch_t.edge_attr).ravel()
 
-    # Moving data to device
-    out_x    = out_x
-    out_attr = out_attr.ravel()
+    node_splits = torch.split(out_x,    g_batch_t.batch.bincount().tolist())  # Split by node count per graph
+    edge_splits = torch.split(out_attr, g_batch_t.edge_index.size(1) // g_batch_t.num_graphs)  # Split by edges per graph
 
     # Construct noise graph with predicted node features and edge attributes, and previous edge indexes
-    idx_x_i    = 0
-    idx_attr_i = 0
-    pred_e_batch_t = []
-    for idx in range(len(g_batch_t)):
-        idx_x_j    = idx_x_i    + g_batch_t[idx].num_nodes
-        idx_attr_j = idx_attr_i + g_batch_t[idx].num_edges
+    # Construct list of `Data` objects with predicted features and edge attributes
+    pred_e_batch_t = [
+        Data(x=node_split, edge_index=g.edge_index, edge_attr=edge_split)
+        for node_split, edge_split, g in zip(node_splits, edge_splits, g_batch_t.to_data_list())
+    ]
 
-        temp_pred = Data(x=out_x[idx_x_i:idx_x_j],
-                         edge_index=g_batch_t[idx].edge_index,
-                         edge_attr=out_attr[idx_attr_i:idx_attr_j])
-
-        # Update indexes
-        idx_x_i    = idx_x_j
-        idx_attr_i = idx_attr_j
-
-        # Append data
-        pred_e_batch_t.append(temp_pred)
-
-    # Generate batch objects
-    pred_e_batch_t = Batch.from_data_list(pred_e_batch_t)
-
-    # Move data to device
-    pred_e_batch_t = pred_e_batch_t.to(device)
-    return pred_e_batch_t
+    # Batch the data in one go and return
+    return Batch.from_data_list(pred_e_batch_t).to(device)
 
 
 def denoising_step(graph_t, epsilon, t, n_t_steps, s, sigma):
