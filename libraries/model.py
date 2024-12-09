2b6489151ffb50ca6992dc4f98852f0fbe2d3ef3
CibranLopez <cibranlpz@gmail.com>
2024-12-09 20:01:16 +0100

    Sigma and alpha decay to float

--- libraries/model.py
+++ libraries/model.py
@@ -272,12 +272,19 @@ def denoise(batch_t, n_t_steps, node_model, edge_model, alpha_decay=1e-2, sigma=
         alpha_t = get_alpha_t(t_step, n_t_steps, alpha_decay)
         batch_0 = denoising_step(batch_0, pred_epsilon_t, alpha_t, sigma, n_features=n_features)
         
-        print()
-        print('Step: ', t_step)
-        print('Alpha: ', alpha_t, 1/torch.sqrt(alpha_t), torch.sqrt((1 - alpha_t) / alpha_t))
-        print(get_random_graph(batch_0.x.size(0), n_features if n_features is not None else batch_0.x.size(1), batch_0.edge_index).x[:5])
-        print('Pred epsilon: ', pred_epsilon_t.x[:5])
-        print('Resulting batch: ', batch_0.x[:5])
+        #print()
+        #print('Step: ', t_step)
+        #print('Alpha: ', alpha_t, 1/torch.sqrt(alpha_t), torch.sqrt((1 - alpha_t) / alpha_t))
+        #print(get_random_graph(batch_0.x.size(0), n_features if n_features is not None else batch_0.x.size(1), batch_0.edge_index).x[:5])
+        #print('Pred epsilon: ', pred_epsilon_t.x[:5])
+        #print('Resulting batch: ', batch_0.x[:5])
+
+        #print()
+        #print('Step: ', t_step)
+        #print('Alpha: ', alpha_t, 1/torch.sqrt(alpha_t), torch.sqrt((1 - alpha_t) / alpha_t))
+        #print(get_random_graph(batch_0.x.size(0), n_features if n_features is not None else batch_0.x.size(1), batch_0.edge_index).edge_attr[:5])
+        #print('Pred epsilon: ', pred_epsilon_t.edge_attr[:5])
+        #print('Resulting batch: ', batch_0.edge_attr[:5])
         
     # Check if intermediate steps are plotted; then, plot the NetworkX graph
     if plot_steps:
